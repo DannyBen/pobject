4baa789a8233f9ed59e3fa7c9da6f6f855543dd3
DannyB <dannyb@telesofia.com>
2016-06-29 18:00:41 +0000

    fix specs to create tmp dir

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,5 +9,10 @@ require 'fileutils'
 require_relative 'fixtures/fixtures'
 
 def reset_tmp
-  FileUtils.rm Dir['spec/tmp/*.{yml,pstore}']
+  Dir.mkdir tmp_dir unless Dir.exist? tmp_dir
+  FileUtils.rm Dir["#{tmp_dir}/*.{yml,pstore}"]
+end
+
+def tmp_dir
+  'spec/tmp'
 end
